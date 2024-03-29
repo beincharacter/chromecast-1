43e0cfe3bff424a8c9f1ebd79ab43d4832189614
shubhampalgaian <shubhamm.pal@gaiansolutions.com>
2024-03-29 21:49:27 +0530

    optmized the code for sending request to devices

--- rsocket.js
+++ rsocket.js
@@ -1,377 +1,202 @@
-const {
-  RSocketClient,
-  JsonSerializer,
-  IdentitySerializer,
-} = require("rsocket-core");
+const { RSocketClient, JsonSerializer, IdentitySerializer } = require("rsocket-core");
 const RSocketTcpClient = require("rsocket-tcp-client").default;
 const nodecastor = require("nodecastor");
+const { Client } = require("castv2-client");
+const { DefaultMediaReceiver } = require("castv2-client");
+const ping = require("ping");
 
-const Client = require("castv2-client").Client;
-const DefaultMediaReceiver = require("castv2-client").DefaultMediaReceiver;
+const CAST_APP_ID = "5CB45E5A";
 
-var client = undefined;
+let client = undefined;
 
-// Create an instance of a client
-client = new RSocketClient({
-  serializers: {
-    data: JsonSerializer,
-    metadata: IdentitySerializer,
-  },
-  setup: {
-    // ms btw sending keepalive to server
-    keepAlive: 6000000,
-    // ms timeout if no keepalive response
-    lifetime: 18000000,
-    // format of `data`
-    dataMimeType: "application/json",
-    // format of `metadata`
-    metadataMimeType: "message/x.rsocket.routing.v0",
-  },
-  transport: new RSocketTcpClient({
-    host: "192.168.28.11",
-    port: 7000,
-  }),
-});
+const setupRSocketClient = () => {
+    client = new RSocketClient({
+        serializers: {
+            data: JsonSerializer,
+            metadata: IdentitySerializer,
+        },
+        setup: {
+            keepAlive: 6000000,
+            lifetime: 18000000,
+            dataMimeType: "application/json",
+            metadataMimeType: "message/x.rsocket.routing.v0",
+        },
+        transport: new RSocketTcpClient({
+            host: "192.168.28.11",
+            port: 7000,
+        }),
+    });
+};
 
-// Open the connection
+const startCasting = async (device) => {
+    try {
+        const { device_name, device_ip, url, x1, x2, y1, y2 } = device;
+        console.log("Casting to device:", device_name);
+        
+
+        let res = await ping.promise.probe(device_ip);
+        console.log("alive : ", res.alive);
+        if(!res.alive) return "Devie not found on network"; 
+        console.log("alive after : ", res.alive);
+
+
+        let scaleFactorParams = "";
+        if (x1 && x2 && y1 && y2) {
+            const scaleParams = calculateScaleParams(x1, x2, y1, y2);
+            scaleFactorParams = `&scalex=${scaleParams.scalex}&scaley=${scaleParams.scaley}&originx=${scaleParams.originx}&originy=${scaleParams.originy}&rotate=0`;
+        }
+
+        let finalUrl = url;
+        finalUrl += `?${scaleFactorParams}&cast=true`;
+
+        const deviceObject = new nodecastor.CastDevice({
+            friendlyName: device_name,
+            name: device_name,
+            address: device_ip,
+        });
+
+        await new Promise((resolve, reject) => {
+            deviceObject.on("connect", () => {
+                deviceObject.status((err, status) => {
+                    if (err) {
+                        console.error("Device status error:", err);
+                        reject(err);
+                        return;
+                    }
+                    console.log("Device status done");
+                    if (device.device && device.device.casterId) {
+                        sendDeviceStatus({ casterId: device.device.casterId, chromecastId: device.host, timestamp: Date.now(), state: "success" });
+                    }
+
+                    try {
+                        deviceObject.application(CAST_APP_ID, (err, application) => {
+                            if (err) {
+                                console.error("Application error:", err);
+                                reject(err);
+                                return;
+                            }
+                            application.run("urn:x-cast:com.url.cast", (err, session) => {
+                                if (err) {
+                                    console.error("Run error:", err);
+                                    reject(err);
+                                    return;
+                                }
+                                session.send({ type: "loc", url: finalUrl }, (err, data) => {
+                                    if (err) {
+                                        console.error("Send error:", err);
+                                        reject(err);
+                                        return;
+                                    }
+                                    console.log("Casting success to device:", device_name);
+                                    resolve();
+                                });
+                            });
+                        });
+                    } catch (error) {
+                        console.error("Casting error:", error);
+                        reject(error);
+                    }
+                });
+            });
+        });
+
+        return "casting done";
+    } catch (error) {
+        console.error("Error in casting:", error);
+    }
+
+};
+
+const stopCasting = (devices) => {
+    console.log("Stopping casting");
+    try {
+        devices.forEach((device) => {
+            const { device_ip } = device;
+            const client = new Client();
+            client.connect(device_ip, () => {
+                client.launch(DefaultMediaReceiver, (err) => {
+                    if (err) {
+                        console.error("Launch error:", err);
+                        return;
+                    }
+                    console.log("Stopped casting to device:", device.device_name);
+                    client.close();
+                });
+            });
+            client.on("error", (err) => {
+                console.error("Error occurred for stopping the casting:", err);
+            });
+        });
+    } catch (error) {
+        console.error("Stop casting error:", error);
+    }
+};
+
+const calculateScaleParams = (x1, x2, y1, y2) => {
+    const scalex = 1 / (x2 - x1);
+    const scaley = 1 / (y2 - y1);
+    const originx = (scalex * x1) / (scalex - 1);
+    const originy = (scaley * y1) / (scaley - 1);
+    return { scalex, scaley, originx, originy };
+};
+
+const sendCasterStatus = (status) => {
+    // Implement sendCasterStatus function
+};
+
+const sendDeviceStatus = (status) => {
+    // Implement sendDeviceStatus function
+};
+
+const continueProcessing = () => {
+    // Implement continueProcessing function
+};
+
+const onNextHandler = () => {
+    
+}
+
+// Main
+setupRSocketClient();
 
 client.connect().subscribe({
-  onComplete: (socket) => {
-    // socket provides the rsocket interactions fire/forget, request/response,
-    // request/stream, etc as well as methods to close the socket.
-    console.log("====connected=====");
-    // console.log(socket)
-    socket.fireAndForget({});
-    socket
-      .requestStream({
-        data: {
-          streamId: "COMMAND",
-          type: "COMMAND",
-          component: "COMMAND",
-        },
-        metadata: "STREAM_REQUEST",
-      })
-      .subscribe({
-        onComplete: () => console.log("complete"),
-        onError: (error) => {
-          console.log("Error occurred:", error);
-          // Handle error and continue
-          continueProcessing();
-        },
-        onNext: (payload) => {
-          let DataObj = JSON.parse(payload.data);
-          console.log("Triggered data :", DataObj);
-          console.log("Cast status : ", DataObj.castStatus);
-
-          if ("StopCast" === DataObj[0].castStatus) {
-            console.log("=========inside stop cast=======");
-            try {
-              stopCastDevice(payload.data);
-            } catch (e) {
-              console.log("Stop cast error : ", e);
-            }
-          } else {
-            console.log("=========inside cast=======");
-            // stopCastDevice(payload.data);
-            CastDevice(payload.data);
-          }
-        },
-        onSubscribe: (subscription) => {
-          subscription.request(2147483647);
-        },
-      });
-  },
-  onError: (error) => {
-    console.log("Connection error:", error);
-    // Handle error and continue
-    continueProcessing();
-  },
-  onSubscribe: (cancel) => {
-    /* call cancel() to abort */
-  },
-});
-
-// Function to start casting
-// function CastDevice(data) {
-//   let castdevices = JSON.parse(data);
-//   console.log("===data====", castdevices);
-
-//   castdevices.forEach(function (device) {
-//     console.log("===device====", device);
-
-//     let deviceName = device.device_name;
-//     let host = device.device_ip;
-//     let url = device.url;
-
-//     console.log("=host==", host);
-
-//     if (device.x1 && device.x2 && device.y1 && device.y2) {
-//       function roundToNearest(number, decimalPlaces) {
-//         const factor = Math.pow(10, decimalPlaces);
-//         return (Math.round(number * factor) / factor).toFixed(decimalPlaces);
-//       }
-//       var x1 = parseFloat(device.x1) / 100;
-//       var x2 = parseFloat(device.x2) / 100;
-//       var y1 = parseFloat(device.y1) / 100;
-//       var y2 = parseFloat(device.y2) / 100;
-
-//       var minusX = x2 - x1;
-//       var minusY = y2 - y1;
-
-//       var scalex = 1 / minusX;
-//       var scalexMinus = scalex - 1;
-//       if (scalexMinus === 0) {
-//         scalexMinus = 1;
-//       }
-
-//       var scaley = 1 / minusY;
-//       var scaleyMinus = scaley - 1;
-//       if (scaleyMinus === 0) {
-//         scaleyMinus = 1;
-//       }
-
-//       var originx = ((scalex * x1) / scalexMinus) * 100;
-//       var originy = ((scaley * y1) / scaleyMinus) * 100;
-
-//       scalex = roundToNearest(scalex, 0);
-//       scaley = roundToNearest(scaley, 0);
-//       originx = roundToNearest(originx, 0);
-//       originy = roundToNearest(originy, 0);
-
-//       let spltUrl = device["url"].split("?");
-//       url =
-//         spltUrl[0] +
-//         "?" +
-//         spltUrl[1] +
-//         `&scalex=${scalex}&scaley=${scaley}&originx=${originx}&originy=${originy}&rotate=0` +
-//         "&cast=true";
-
-//       console.log("url : ", url);
-//     }
-
-//     console.log("url--", url);
-
-//     if (data.device && data.device.casterId) {
-//       var timeStamp = new Date().getTime();
-//       sendCasterStatus({
-//         casterId: data.device.casterId,
-//         timestamp: timeStamp,
-//         state: "running",
-//       });
-//     }
-//     var device = new nodecastor.CastDevice({
-//       friendlyName: deviceName,
-//       name: deviceName,
-//       address: host,
-//     });
-
-//     device.on("connect", () => {
-//       device.status((err, status) => {
-//         if (err) {
-//           console.log(err);
-//           return;
-//         }
-//         console.log("=======status done=======");
-//         if (data.device && data.device.casterId) {
-//           var timeStamp = new Date().getTime();
-//           sendDeviceStatus({
-//             casterId: data.device.casterId,
-//             chromecastId: device.host,
-//             timestamp: timeStamp,
-//             state: "success",
-//           });
-//         }
-
-//         return new Promise(function (resolve, reject) {
-//           try {
-//             device.application("5CB45E5A", (err, application) => {
-//               if (err) {
-//                 console.log(err);
-//                 return;
-//               }
-//               application.run("urn:x-cast:com.url.cast", (err, session) => {
-//                 if (err) {
-//                   console.log(err);
-//                   return;
-//                 }
-//                 session.send({ type: "loc", url: url }, (err, data) => {
-//                   console.log(data);
-//                   resolve();
-//                   return;
-//                 });
-//               });
-//             });
-//           } catch (error) {
-//             console.log(error);
-//             reject(error);
-//             return;
-//           }
-//         });
-//       });
-//     });
-//   });
-// }
-
-// Function to start casting
-async function CastDevice(data) {
-  try {
-    let castdevices = JSON.parse(data);
-
-    for (const device of castdevices) {
-      console.log("Casting to device:", device.device_name);
-
-      let deviceName = device.device_name;
-      let host = device.device_ip;
-      let url = device.url;
-
-      
-
-    if (device.x1 && device.x2 && device.y1 && device.y2) {
-      function roundToNearest(number, decimalPlaces) {
-        const factor = Math.pow(10, decimalPlaces);
-        return (Math.round(number * factor) / factor).toFixed(decimalPlaces);
-      }
-      var x1 = parseFloat(device.x1) / 100;
-      var x2 = parseFloat(device.x2) / 100;
-      var y1 = parseFloat(device.y1) / 100;
-      var y2 = parseFloat(device.y2) / 100;
-
-      var minusX = x2 - x1;
-      var minusY = y2 - y1;
-
-      var scalex = 1 / minusX;
-      var scalexMinus = scalex - 1;
-      if (scalexMinus === 0) {
-        scalexMinus = 1;
-      }
-
-      var scaley = 1 / minusY;
-      var scaleyMinus = scaley - 1;
-      if (scaleyMinus === 0) {
-        scaleyMinus = 1;
-      }
-
-      var originx = ((scalex * x1) / scalexMinus) * 100;
-      var originy = ((scaley * y1) / scaleyMinus) * 100;
-
-      scalex = roundToNearest(scalex, 0);
-      scaley = roundToNearest(scaley, 0);
-      originx = roundToNearest(originx, 0);
-      originy = roundToNearest(originy, 0);
-
-      let spltUrl = device["url"].split("?");
-      url =
-        spltUrl[0] +
-        "?" +
-        spltUrl[1] +
-        `&scalex=${scalex}&scaley=${scaley}&originx=${originx}&originy=${originy}&rotate=0` +
-        "&cast=true";
-
-      console.log("url : ", url);
-    }
-
-
-      if (data.device && data.device.casterId) {
-        var timeStamp = new Date().getTime();
-        sendCasterStatus({
-          casterId: data.device.casterId,
-          timestamp: timeStamp,
-          state: "running",
-        });
-      }
-
-      var deviceObject = new nodecastor.CastDevice({
-        friendlyName: deviceName,
-        name: deviceName,
-        address: host,
-      });
-
-      await new Promise((resolve, reject) => {
-        deviceObject.on("connect", () => {
-          deviceObject.status((err, status) => {
-            if (err) {
-              console.log(err);
-              reject(err);
-              return;
-            }
-
-            console.log("Device status done");
-            if (data.device && data.device.casterId) {
-              var timeStamp = new Date().getTime();
-              sendDeviceStatus({
-                casterId: data.device.casterId,
-                chromecastId: device.host,
-                timestamp: timeStamp,
-                state: "success",
-              });
-            }
-
-            try {
-              deviceObject.application("5CB45E5A", (err, application) => {
-                if (err) {
-                  console.log(err);
-                  reject(err);
-                  return;
-                }
-                application.run("urn:x-cast:com.url.cast", (err, session) => {
-                  if (err) {
-                    console.log(err);
-                    reject(err);
-                    return;
-                  }
-                  session.send({ type: "loc", url: url }, (err, data) => {
-                    if (err) {
-                      console.log(err);
-                      reject(err);
-                      return;
+    onComplete: (socket) => {
+        console.log("Connected");
+        socket.fireAndForget({});
+        socket.requestStream({
+            data: { streamId: "COMMAND", type: "COMMAND", component: "COMMAND" },
+            metadata: "STREAM_REQUEST",
+        }).subscribe({
+            onComplete: () => console.log("Complete"),
+            onError: (error) => {
+                console.error("Error occurred:", error);
+                continueProcessing();
+            },
+            onNext: async (payload) => {
+                const dataObj = JSON.parse(payload.data);
+                console.log("Triggered data:", dataObj.length);
+                console.log("Cast status:", dataObj.castStatus);
+                if (dataObj[0]?.castStatus === "StopCast") {
+                    console.log("Stopping cast");
+                    stopCasting(JSON.parse(payload.data));
+                } else {
+                  for(let i=0; i<dataObj.length; i++) {
+                      console.log("Starting cast");
+                      let res = startCasting(dataObj[i]);
+                      console.log(res);
                     }
-                    console.log("Casting success to device:", deviceName);
-                    resolve();
-                  });
-                });
-              });
-            } catch (error) {
-              console.log(error);
-              reject(error);
-              return;
-            }
-          });
+                }
+            },
+            onSubscribe: (subscription) => {
+                subscription.request(2147483647);
+            },
         });
-      });
-    }
-  } catch (error) {
-    console.log("Error in casting:", error);
-  }
-}
-
-
-//function to stop casting
-function stopCastDevice(data) {
-  console.log("inside stop cast")
-  try {
-
-  let castdevices = JSON.parse(data);
-  console.log("===data====", castdevices);
-
-  castdevices.forEach(function (device) {
-    let host = device.device_ip;
-    console.log("=host==", host);
-
-    const client = new Client();
-
-    client.connect(host, () => {
-      client.launch(DefaultMediaReceiver, (err, player) => {
-        // player.stop();
-        client.close();
-      });
-    });
-
-    client.on("error", (err) => {
-      console.log("Error occurred for stop the casting:", err);
-    });
-  });
-    
-  } catch (error) {
-    console.log("stopped : ", error)
-  }
-}
+    },
+    onError: (error) => {
+        console.error("Connection error:", error);
+        continueProcessing();
+    },
+    onSubscribe: (cancel) => {
+        /* call cancel() to abort */
+    },
+});
